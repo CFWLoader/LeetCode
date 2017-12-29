603be690c98132895414cd1d7da91fe0c1464e85
Evan Huang <cfwloader@gmail.com>
2017-12-29 11:26:44 +0800

    Finally reviewed Dynamic Programming.

--- Problem718/Solution.hpp
+++ Problem718/Solution.hpp
@@ -18,35 +18,41 @@ public:
             return 0;
         }
 
-        vector<vector<int>> memo(A.size());
+        vector<vector<int>> memo(A.size(), vector<int>(B.size()));
 
-        for(int idx = 0; idx < memo.size(); ++idx)
+        for(int i = 0; i < memo.size(); ++i) memo[i][0] = A[i] == B[0] ? 1 : 0;
+        for(int j = 0; j < memo[0].size(); ++j) memo[0][j] = A[0] == B[j] ? 1 : 0;
+
+        int maxVal = 0;
+
+        for(int i = 1; i < memo.size(); ++i)
         {
-            memo[idx].resize(B.size());
-        }
-
-        int upVal, leftVal, maxMatched = 0;
-
-        for(int i = 0; i < memo.size(); ++i)
-        {
-            for(int j = 0; j < memo[i].size(); ++j)
+            for(int j = 1; j < memo[i].size(); ++j)
             {
-                upVal = i - 1 >= 0 ? memo[i - 1][j] : 0;
+                if(A[i] == B[j])
+                {
+                    memo[i][j] = memo[i - 1][j - 1] + 1;
 
-                leftVal = j - 1 >= 0 ? memo[i][j - 1] : 0;
-
-                memo[i][j] = (upVal > leftVal ? upVal : leftVal) + (A[i] == B[j] ? 1 : 0);
-
-                memo[i][j] = memo[i][j] > i + 1 ? i + 1 : memo[i][j];
-
-//                std::cout << "(" << i << "," << j << "," << "A[" << A[i] << "],B["<< B[j] << "]," << memo[i][j] << ") ";
-
+                    if(memo[i][j] > maxVal)
+                    {
+//                        std::cout << "New max:" << memo[i][j] << std::endl;
+                        maxVal = memo[i][j];
+                    }
+                }
             }
 
-//            std::cout << std::endl;
         }
 
-        return memo[A.size() - 1][B.size() - 1];
+//        for(int i = 0; i < memo.size(); ++i)
+//        {
+//            for(int j = 0; j < memo[i].size(); ++j)
+//            {
+//                std::cout << "(" << i << "," << j << "," << "A[" << A[i] << "],B["<< B[j] << "]," << memo[i][j] << ") ";
+//            }
+//            std::cout << std::endl;
+//        }
+
+        return maxVal;
 
     }
 };
