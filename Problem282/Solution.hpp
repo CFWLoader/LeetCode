70c436a4f68a026541c93a92e34605ade3bb5895
Evan Huang <cfwloader@gmail.com>
2017-12-29 11:33:39 +0800

    Start solving next problem.

---
+++ Problem282/Solution.hpp
@@ -0,0 +1,15 @@
+//
+// Created by CFWLoader on 12/29/17.
+//
+
+#ifndef PROBLEM282_SOLUTION_HPP
+#define PROBLEM282_SOLUTION_HPP
+
+class Solution {
+public:
+    vector<string> addOperators(string num, int target) {
+
+    }
+};
+
+#endif //PROBLEM282_SOLUTION_HPP
